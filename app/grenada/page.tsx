c3a8739dcf470b34cdad30ea6b022a7ed86b31f3
Adam Marsala <adammarsala@hotmail.com>
2023-08-19 17:46:30 -0400

    set up basic styles and pages

---
+++ app/grenada/page.tsx
@@ -0,0 +1,21 @@
+// Grenada page
+
+export default function Grenada() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="bg-white rounded-[28px] drop-shadow-lg text-blue z-10 w-[90%] max-w-[600px] p-5 flex flex-col items-center justify-center lg:flex">
+        <h1 className="text-4xl font-bold">Grenada</h1>
+        <h2 className="text-3xl">May 9, 2024</h2>
+        <br />
+        <strong>Location:</strong>
+        <p>True Blue Bay Resort, St. George&apos;s, Grenada</p>
+        <br />
+        <strong>Details:</strong>
+        <p>
+          We&apos;re still working out the details, but we&apos;ll be sharing
+          more information as we get closer to the date!
+        </p>
+      </div>
+    </main>
+  );
+}
