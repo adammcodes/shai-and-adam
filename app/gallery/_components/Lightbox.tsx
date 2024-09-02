666defa4d754e5ce9f3c20f6399160b908403312
Adam Marsala <adammarsala@hotmail.com>
2024-09-02 14:37:00 -0400

    Use wheel or scroll actions to trigger next or prev image in lightbox with debouncing at 100ms

--- app/gallery/_components/Lightbox.tsx
+++ app/gallery/_components/Lightbox.tsx
@@ -1,5 +1,5 @@
 // components/Lightbox.tsx
-import React, { useEffect, useCallback, use } from "react";
+import React, { useEffect, useCallback } from "react";
 import B2Image from "./B2Image";
 
 interface LightboxProps {
@@ -40,6 +40,53 @@ const Lightbox: React.FC<LightboxProps> = ({
       document.body.style.overflow = "auto";
     };
   }, []);
+
+  // detect side scrolling and trigger prev/next while debouncing
+  // useEffect(() => {
+  //   const handleWheel = (e: WheelEvent) => {
+  //     if (e.deltaY > 0) onNext();
+  //     if (e.deltaY < 0) onPrev();
+  //   };
+
+  //   document.addEventListener("wheel", handleWheel);
+  //   return () => document.removeEventListener("wheel", handleWheel);
+  // }, [onNext, onPrev]);
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | null = null;
+    let lastWheelTime = 0;
+    const wheelDelay = 100; // Adjust this value to fine-tune the debounce delay
+
+    const handleWheel = (e: WheelEvent) => {
+      const currentTime = new Date().getTime();
+
+      if (currentTime - lastWheelTime < wheelDelay) {
+        // If the wheel event is triggered within the delay, clear the previous timeout
+        if (timeoutId) clearTimeout(timeoutId);
+      } else {
+        // If it's a new wheel action, update the last wheel time
+        lastWheelTime = currentTime;
+      }
+
+      // Set a new timeout
+      timeoutId = setTimeout(() => {
+        if (Math.abs(e.deltaX) > Math.abs(e.deltaY)) {
+          // Horizontal scrolling
+          if (e.deltaX > 0) onNext();
+          else if (e.deltaX < 0) onPrev();
+        } else {
+          // Vertical scrolling
+          if (e.deltaY > 0) onNext();
+          else if (e.deltaY < 0) onPrev();
+        }
+      }, wheelDelay);
+    };
+
+    document.addEventListener("wheel", handleWheel, { passive: true });
+    return () => {
+      document.removeEventListener("wheel", handleWheel);
+      if (timeoutId) clearTimeout(timeoutId);
+    };
+  }, [onNext, onPrev]);
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
